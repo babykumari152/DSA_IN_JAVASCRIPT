fe81b2cab330b574477d68806f335549a9cda0a9
babykumari152 <bkumari9715@gmail.com>
2023-06-08 08:20:21 +0530

    subset string

--- RECURSION/SubsetString.js
+++ RECURSION/SubsetString.js
@@ -1,3 +1,16 @@
-function SubsetString(str){
-    
+function SubsetString(subset,str){
+    if(!str){
+        let list = [];
+        if(subset)
+         list.push(subset);
+        return list;  
+    }
+    let ch = str.charAt(0);
+    let left = SubsetString(subset+ch,str.substring(1));
+    let right = SubsetString(subset,str.substring(1));
+    left = left.concat(right);
+    return left;
+
 }
+
+console.log(SubsetString('','abc'));
